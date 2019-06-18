ae5c226180e269b874407189c5e11ec9ba0bd4ee
JoamirS <jmrsilveira@outlook.com>
2019-06-18 15:15:11 -0300

    Celula da Árvore Binária
    Classe com as informações do nó da célula

---
+++ EstruturaDeDados/CelulaArvore.java
@@ -0,0 +1,24 @@
+package EstruturaDeDados;
+
+/**@author Joamir Silveira*/
+
+/* A classe Celula se chama CelulaArvore, pois a celula de Lista já possui o nome
+de Celula. */
+
+
+/* Foi declarado o dado, e o apontamentos das 'caixinhas' da celula semelhante
+    ao desenho abaixo. 
+        |-----------------------------|
+-->>    | esquerda  | dado |  direita |
+        |-----------------------------|
+*/
+
+public class CelulaArvore {
+    int dado;
+    CelulaArvore esquerda;
+    CelulaArvore direita;
+    
+}
+/* Fonte de Pesquisa: 
+Github.com, Apostila Caelum Estrutura de Dados, Wikipedia.com 
+https://www.ime.usp.br/~song/mac5710/slides/05tree */
